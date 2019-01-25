fd9743de0b456487d112772c45c33cc2cc60077b
Christoph Gruener <christophgruener@me.com>
2019-01-25 16:42:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(integer)
   if integer < 2 
     false
   else
-    if range.map { |x| integer / x } == range.map { |x| x = 0 } 
+    if range.map { |x| integer / x } <=> range.map { |x| x = 0 } 
      true
     elsif
      false
